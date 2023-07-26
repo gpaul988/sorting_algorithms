c99ee18947fa58cc961dd44237f8562c52efe468
gpaul988 <gpaul988@gmail.com>
2023-07-25 19:22:37 -0700

    selection fixed

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -16,30 +16,35 @@ void swap(int *xp, int *yp)
 }
 
 /**
- * selection_sort - Arrange group of integers in increasing order using
- * the selection sort algorithm
- *
- * @array: Group to be arranged
- * @size: Nature of the group
- */
+* selection_sort - Arrange group of integers in increasing order using
+* the selection sort algorithm
+*
+* @array: Group to be arranged
+* @size: Nature of the group
+*/
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min_idx;
+size_t i, j, min_idx;
+int temp;
 
-	if (size < 2)
-		return;
+if (array == NULL || size <= 1)
+return;
 
-	for (i = 0; i < size - 1; i++)
-	{
-		min_idx = i;
-		for (j = i + 1; j < size; j++)
-		{
-			if (array[j] < array[min_idx])
-				min_idx = j;
-		}
-		if (min_idx == i)
-			continue;
-		swap(&array[min_idx], &array[i]);
-		print_array(array, size);
-	}
+for (i = 0; i < size - 1; i++)
+{
+min_idx = i;
+for (j = i + 1; j < size; j++)
+{
+if (array[j] < array[min_idx])
+min_idx = j;
+}
+
+if (min_idx != i)
+{
+temp = array[i];
+array[i] = array[min_idx];
+array[min_idx] = temp;
+print_array(array, size);
+}
+}
 }
